c01f1cd6ac2c0f5e08e831c78d5c16d3ec720e84
chraul22 <c.auld@student.curtin.edu.au>
2023-05-27 16:16:51 +1000

    redeploy heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 const fileUpload = require('express-fileupload')
 
 
